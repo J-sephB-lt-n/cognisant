512467c325d0d2af13dcbc0d6eac13a9fa4e87f2
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2022-01-27 16:11:04 +0200

    more concept planning changes

--- README.md
+++ README.md
@@ -10,24 +10,24 @@ import numpy as np
 
 analysis_obj = cognisant.analysis_engine()
 
-analysis_obj.prepare_data(
+analysis_obj.load_dataset(
 							data = {
 									"day":{
 												"vbl_function":"time_index"
 											,	"vbl_tags":None
-											,	"vbl_values":list(range(100))
+											,	"vbl_values":list(range(35))
 									}
 									,
 									"sales":{
 												"vbl_function":"to_maximise"
 											,	"vbl_tags":None
-											,	"vbl_values":np.random.randint()
+											,	"vbl_values":np.random.uniform(low=100,high=1000,size=35)
 									}
 									,
 									"day_of_week":{
 												"vbl_function":"covariate"
 											,	"vbl_tags":None
-											,	"vbl_values":[]
+											,	"vbl_values":["mon","tues","wed","thu","fri","sat","sun"]*5
 									}
 							}
 )
@@ -35,11 +35,18 @@ analysis_obj.prepare_data(
 import json
 print( "## AVAILABLE TESTS ##" )
 print( json.dumps(analysis_obj.available_tests, indent=4) )
+# TODO: give ability to showed filtered list of tests by constraint (same filters as those in generate_insight() below)
+# TODO: give ability to showed filtered list of variables by constraint (same filters as those in generate_insight() below)
 
+# run a specific test:
+analysis_obj.generate_insight( list_of_allowed_tests=["autocorrelation"] ) 
+
+# generate a random insight (optionally with user-specified constraints)
 analysis_obj.generate_insight(
 			insight_must_contain_AT_LEAST_ONE_of_these_variables = []
 		,	insight_must_contain_ALL_of_these_variables = []
-		,	
+		,	insight_must_NOT_contain_ANY_of_these_variables = []
+		,	list_of_allowed_tests = []
 		,	test_must_have_AT_LEAST_ONE_of_these_tags = [
 						"univariate"
 					,	"multivariate"
@@ -47,5 +54,14 @@ analysis_obj.generate_insight(
 					,	"predictive_model"
 			]
 		,	test_must_have_ALL_of_these_tags = []
+		,	test_must_NOT_contain_ANY_of_these_tags = []
+		,	minimum_test_significance_level = "medium"
 	)
+
+# view and edit the significance thresholds of one of the tests:
+
+
+# after running many tests, free up memory by removing history of past tests:
+analysis_obj.clear_test_history()
+
 ```
--- scripts/cognisant.py
+++ scripts/cognisant.py
@@ -1,25 +1,56 @@
 
 class analysis_engine():
     def __init__(self):
+        self.test_history = []          # each time a test is done, the result is appended to this list
         self.available_tests = {
                 "autocorrelation":{ 
-                                        "tags":["correlation","linear","predictive","time_series","univariate"] 
+                                        "tags":set( ["correlation","linear","predictive","time_series","univariate"] ) 
                                     ,   "description":"describes the strength of the linear relationship (correlation) between a variable and a lagged version of itself"
+                                    ,   "significance_thresholds":{
+                                                "metric":"pearson_correlation"
+                                            ,   "thresholds":{"low":0.1, "medium":0.5, "high":0.8}
+                                    }
                 }
             ,   "single_linear_leading_indicator":{ 
-                                        "tags":["bivariate","correlation","linear","predictive","time_series"]
+                                        "tags":set( ["bivariate","correlation","linear","predictive","time_series"] )
                                     ,   "description":"measures the significance of the linear relationship between 2 variables, where one measurement precedes the other in time" 
+                                    ,   "significance_thresholds":{
+                                                "metric":"pearson_correlation"
+                                            ,   "thresholds":{"low":0.1, "medium":0.5, "high":0.8}
+                                    }                                    
                 }
-            ,   "single_nonlinear_leading_indicator_using_cubic_splines":{ 
-                                        "tags":["bivariate","non_linear","predictive","splines","time_series"]
+            ,   "single_nonlinear_leading_indicator_cubic_spline_prediction":{ 
+                                        "tags":set( ["bivariate","non_linear","predictive","splines","time_series"] )
                                     ,   "description":"measures the significance of a non-linear relationship (modelled with cubic splines) between 2 variables, where one measurement precedes the other in time" 
+                                    ,   "significance_thresholds":{
+                                                "metric":"1 / absolute_value(mean_absolute_error/mean_observed_value)"
+                                            ,   "thresholds":{"low":1/0.1, "medium":1/0.05, "high":1/0.01}
+                                    }
                 }                
         }
     
+    def clear_test_history(self):
+        # TODO: allow clearing of 
+        self.test_history = []
+
     def load_dataset( 
-                self
+                    self
+                ,   dataset
             ):
+        # TODO: some tests to validate format of input data
+        self.data = dataset
+
+    def run_unit_tests( self, tests_to_run=[], **kwargs ):
         pass
-        # define self.available_tests list based on the types of data available
 
-
+    def run_test( self, test_name ):
+        if test_name not in self.available_tests:
+            print( f"'{test_name}' is not an available test" )
+        elif test_name == "autocorrelation":
+            print( f"'autocorrelation' test is not implemented yet")
+        elif test_name == "single_linear_leading_indicator":
+            print( f"'single_linear_leading_indicator' test is not implemented yet")
+        elif test_name == "single_nonlinear_leading_indicator_using_cubic_splines":
+            print( f"'single_nonlinear_leading_indicator_using_cubic_splines' test is not implemented yet")
+        else:
+            pass
